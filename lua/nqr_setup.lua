d5cf508ae813f2c475e77608480499d446856d31
DR01D_Qs <gikqs550@gmail.com>
2026-05-15 13:24:25 +0300

    0.1.27.1
     - Fixed Team AI always being disabled even after enabling it.

--- lua/nqr_setup.lua
+++ lua/nqr_setup.lua
@@ -1,3 +1,22 @@
-Hooks:PostHook(Setup, "init_managers", "nqr_Setup:init_managers", function(self, managers)
-	Global.game_settings.team_ai = false
+Hooks:PreHook(Setup, "init_managers", "nqr_Setup:init_managers", function(self, managers)
+	Global.game_settings = Global.game_settings or {
+		is_playing = false,
+		auto_kick = true,
+		drop_in_option = 1,
+		permission = "public",
+		job_plan = -1,
+		search_modded_lobbies = true,
+		search_appropriate_jobs = true,
+		gamemode = "standard",
+		drop_in_allowed = true,
+		reputation_permission = 0,
+		difficulty = "normal",
+		team_ai_option = 1,
+		--team_ai = false,
+		kick_option = 1,
+		one_down = false,
+		allow_modded_players = true,
+		search_one_down_lobbies = false,
+		level_id = managers.dlc:is_trial() and "bank_trial" or "branchbank"
+	}
 end)
