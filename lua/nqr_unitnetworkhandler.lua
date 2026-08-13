4b0f495b6c31f38103a5b03541008f55a8145e89
DR01D_Qs <gikqs550@gmail.com>
2026-08-14 01:42:25 +0300

    0.2.1.2
    13.08.2026, Alpha 0.2.1.2:
     - U247/Diesel 3.0/64bit compatibility hotfix:
    	 - CRASH FIX: when loading into a heist.
    	 - (potential) CRASH FIX: when another player loots a corpse for ammo.

--- lua/nqr_unitnetworkhandler.lua
+++ lua/nqr_unitnetworkhandler.lua
@@ -1,22 +1,20 @@
 --RECOVER UNIT FROM UNIT_ID IF ITS DEAD WHEN NQR_CORPSE_LOOT
 function UnitNetworkHandler:sync_interacted(unit, unit_id, tweak_setting, status, sender)
+	log("UnitNetworkHandler:sync_interacted")
 	if not self._verify_gamestate(self._gamestate_filter.any_ingame) then return end
+
 	local peer = self._verify_sender(sender)
 	if not peer then return end
+
+	local char_unit = managers.criminals:character_unit_by_peer_id(peer:id())
+	if alive(unit) and char_unit and not tweak_setting then
+		managers.player:sync_interacted(peer, char_unit, status)
+		return
+	end
 
 	local unit = unit
 	if tweak_setting=="nqr_corpse_loot" and not unit then
 		unit = managers.enemy:get_corpse_unit_data_from_id(unit_id) and managers.enemy:get_corpse_unit_data_from_id(unit_id).unit or unit
-	end
-
-	if Network:is_server() and unit_id ~= -2 then
-		if alive(unit) and unit:interaction() and unit:interaction().tweak_data == tweak_setting and unit:interaction():active() then
-			sender:sync_interaction_reply(true)
-		else
-			sender:sync_interaction_reply(false)
-
-			return
-		end
 	end
 
 	if alive(unit) and unit:interaction() then
